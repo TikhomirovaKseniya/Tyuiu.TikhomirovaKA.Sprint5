cbe554dd8a6b7e553e09ee7de12559ad5cd23deb
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-24 16:50:49 +0500

    Task5

--- Tyuiu.TikhomirovaKA.Sprint5.Task5.V14.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint5.Task5.V14.Lib/DataService.cs
@@ -1,7 +1,37 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint5.Task5.V14.Lib
-{
-    public class DataService
-    {
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
+namespace Tyuiu.TikhomirovaKA.Sprint5.Task5.V14.Lib
+{
+    public class DataService : ISprint5Task5V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+            string[] numbers = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int minNumber = int.MinValue;
+            foreach (string numberStr in numbers)
+            {
+                double number = double.Parse(numberStr, CultureInfo.InvariantCulture);
+                if (number == Math.Truncate(number) && number % 3 == 0)
+                {
+                    int intNumber = (int)number;
+                    if (intNumber > minNumber)
+                    {
+                        minNumber = intNumber;
+                    }
+                }
+            }
+            if (minNumber == int.MinValue)
+                return 0;
+            long factorial = 1;
+            for (int i = 2; i <= minNumber; i++)
+            {
+                factorial *= i;
+            }
+            double res = Math.Round((double)factorial, 3);
+            return res;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint5.Task5.V14/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint5.Task5.V14/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint5.Task5.V14.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+
+        string path = Path.Combine("InPutDataFileTask5V14.txt");
+        Console.WriteLine(path);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
