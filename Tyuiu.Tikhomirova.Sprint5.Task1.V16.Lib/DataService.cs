c9166174955092f81fab70ddd376a79243aa5809
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-24 14:18:29 +0500

    Task1

---
+++ Tyuiu.Tikhomirova.Sprint5.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.Tikhomirova.Sprint5.Task1.V16.Lib
+{
+    public class DataService : ISprint5Task1V16
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.GetTempPath() + Path.Combine("OutPutFileTask1.txt");
+            if (File.Exists(path)) File.Delete(path);
+            startValue = -5;
+            stopValue = 5;
+            for (double x = startValue; x <= stopValue; x++)
+            {
+                double fx = Math.Round((Math.Sin(x) + (2 * x) / 3 - Math.Cos(x) * 4 * x), 2);
+                if (double.IsInfinity(fx) || double.IsNaN(fx))
+                {
+                    fx = 0;
+                }
+                if (x != stopValue) File.AppendAllText(path, Convert.ToString(fx) + Environment.NewLine);
+                else File.AppendAllText(path, Convert.ToString(fx));
+            }            
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.Tikhomirova.Sprint5.Task1.V16.Test/DataServicetest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.Tikhomirova.Sprint5.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServicetest
+    {
+        [TestMethod]
+        public void CheckedExistFile()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileinfo = new FileInfo(path);
+            bool wait = true;
+            Assert.AreEqual(wait, fileinfo.Exists);
+        }
+    }
+}
