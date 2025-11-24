c948ad38148436dbf7f50df2d8ce2009834c7d26
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-24 16:25:45 +0500

    Task4

--- Tyuiu.TikhomirovaKA.Sprint5.Task4.V6.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint5.Task4.V6.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.TikhomirovaKA.Sprint5.Task4.V6.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string x = File.ReadAllText(path); 
+            string x = File.ReadAllText(path);            
             double res = Math.Round((1/Math.Cos(Convert.ToDouble(x))+2.2*Math.Pow(Convert.ToDouble(x), 2)), 3);            
             return res;
         }
