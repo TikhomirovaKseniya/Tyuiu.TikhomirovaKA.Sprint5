b2c43189ba0528e84319eba63fc723615766c0d5
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-24 17:20:17 +0500

    Task7

--- Tyuiu.TikhomirovaKA.Sprint5.Task7.V13.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint5.Task7.V13.Lib/DataService.cs
@@ -1,7 +1,21 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint5.Task7.V13.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.TikhomirovaKA.Sprint5.Task7.V13.Lib
+{
+    public class DataService : ISprint5Task7V13
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string str = File.ReadAllText(path);
+            string newStr = "";
+            foreach (char c in str)
+            {
+                if (!(c >= 'a' && c <= 'z')) newStr += c;
+            }
+            string file = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V13.txt");
+            if (File.Exists(file)) File.Delete(file);
+            File.WriteAllText(file, newStr);
+            return file;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint5.Task7.V13/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint5.Task7.V13/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint5.Task7.V13.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+
+        string path = Path.Combine("InPutDataFileTask7V13.txt");
+        Console.WriteLine(path);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        string res = ds.LoadDataAndSave(path);
+        Console.WriteLine("Файл: " + res + " создан.");
+        Console.ReadKey();
+    }
+}
